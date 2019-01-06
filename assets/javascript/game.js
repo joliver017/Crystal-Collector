43636dbdb9696e6f6e76c8e6553415f331af9a5e
Oliver 2wist <joliver017@gmail.com>
2019-01-06 10:33:58 -0600

    edit some styling, added code to choose random number and random value for crystals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,21 @@
-var randomNumber = Math.floor(Math.random() * 101+19);
+var randomNumber = Math.floor(Math.random() * 102) + 19;
+var totalScore = 0;
 var totalWins = 0;
 var totalLosses = 0;
-var totalScore = 0;
 
-console.log(randomNumber);
+
+var value_1 = Math.floor(Math.random() * 12) + 1;
+var value_2 = Math.floor(Math.random() * 12) + 1;
+var value_3 = Math.floor(Math.random() * 12) + 1;
+var value_4 = Math.floor(Math.random() * 12) + 1;
+
+console.log("random number = " + randomNumber);
+console.log(value_1);
+console.log(value_2);
+console.log(value_3);
+console.log(value_4);
+
+// This randomly selects and writes a random number when doc is ready
+$( document ).ready(function(){
+$("#currentNumber").text(randomNumber);
+});
